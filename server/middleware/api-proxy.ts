569157c731270cdcc8bc7d6bafb69ae03789aafd
harasya <harasya.email@gmail.com>
2024-11-20 23:17:32 +0700

    fix netlify api function

--- server/middleware/api-proxy.ts
+++ server/middleware/api-proxy.ts
@@ -3,28 +3,28 @@ import { createProxyMiddleware } from 'http-proxy-middleware';
 export default defineEventHandler(async (event) => {
     const {apiBaseUrl} = useRuntimeConfig();
 
-   // Hanya jalankan middleware jika URL diawali dengan `/api`
     if (!event.node.req.url?.startsWith('/api')) {
-        return; // Abaikan middleware
+        return;
     }
 
-    // Initialize the proxy middleware
     const proxy = createProxyMiddleware({
-        target: apiBaseUrl, // Use the API base URL from the environment variable
-        changeOrigin: true, // Changes the origin of the host header to the target URL
+        target: apiBaseUrl,
+        changeOrigin: true,
         pathRewrite: {
-        '^/api': '', // Remove `/api` from the proxied request path
+        '^/api': '',
         },
     });
 
-    // Forward the request using the proxy middleware
     return new Promise<void>((resolve, reject) => {
         proxy(event.node.req, event.node.res, (err) => {
-        if (err) {
-            reject(err);
-        } else {
-            resolve();
-        }
+            if (err) {
+                console.error("Proxy error:", err);
+                event.node.res.statusCode = 500;
+                event.node.res.end(JSON.stringify({ error: "Proxy failed" }));
+                reject(err);
+            } else {
+                resolve();
+            }
         });
     });
 });
